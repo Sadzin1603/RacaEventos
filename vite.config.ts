0fd2af58d4c08ffdf4b563843fea5aec0583af0c
Sadzin1603 <kauanfeliperossi@gmail.com>
2026-08-12 14:17:27 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -80,7 +80,7 @@ function figmaSiteConfiguration(config: FigmaSiteConfiguration): Plugin {
     return html.replace(`<!-- ${slotName} -->`, content)
   }
 
-  const title = config.title ?? "Figma Make App"
+  const title = config.title ?? "Raca Eventos"
   const description = config.description ?? ''
   const favicon = config.icons?.icon ?? ''
   const socialImage = config.openGraph?.image ?? ''
